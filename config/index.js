0cb9b49d4e8e50c5823d6b6a06384ecfeffb284e
葫芦不是瓢 <1563346609@qq.com>
2022-08-27 09:47:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "LN8qG1TxrhHRODK4D4kp2qxrBtUbRr3AGvamSsagTEw",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [oYR7L55gn6fafuDVNu1rxEFbcqXE],
+    user: ["oYR7L55gn6fafuDVNu1rxEFbcqXE"],
      
     // 信息配置
     // 所在省份
